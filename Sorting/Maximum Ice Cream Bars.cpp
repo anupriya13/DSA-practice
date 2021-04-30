3dd5652ab9425a0af9800e4c8f8213e5382465e7
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2021-04-30 12:19:50 +0530

    Create Maximum Ice Cream Bars.cpp
    https://leetcode.com/problems/maximum-ice-cream-bars/

---
+++ Sorting/Maximum Ice Cream Bars.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        
+        int i;
+        for(i = 0; i < costs.size(); i++)
+        {
+            coins -= costs[i];
+            if(coins < 0)
+                return i;
+        }
+        
+        return i;
+    }
+};
